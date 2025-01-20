7e6ccae41adc434d48ee01d1c439214d44a0598a
RickvEngelenburg <180569491+RickvEngelenburg@users.noreply.github.com>
2025-01-20 12:54:55 +0100

    Changed graph, made part of presentation

--- code/visualisation/graphs.py
+++ code/visualisation/graphs.py
@@ -1,9 +1,16 @@
 import matplotlib.pyplot as plt
 
 def graph(random_scores):
-    bins = range(0, 10001, 1000)
+    bins = range(0, 10001, 500)
     
-    plt.hist(random_scores, bins = bins, edgecolor = "black")
+    counts, edges, patches = plt.hist(random_scores, bins = bins, edgecolor = "black")
+    
+    for count, edge in zip(counts, edges[:-1]):
+        if count > 0:
+            plt.text(edge + 250, count + 0.5, f"{int(count)}", ha='center', fontsize=10)
+    
+    plt.xlim(0, 10000)
+    plt.ylim(0, None)
     
     plt.xlabel("Score (K)")
     plt.ylabel("Count")
